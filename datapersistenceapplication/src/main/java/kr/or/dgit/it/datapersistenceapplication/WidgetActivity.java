76eef3e9444aa44ac76f10378950c97a9d6080d0
hanseok Park <es12850@naver.com>
2018-05-01 12:42:48 +0900

    11장 pdf 13쪽 하는중

---
+++ datapersistenceapplication/src/main/java/kr/or/dgit/it/datapersistenceapplication/WidgetActivity.java
@@ -0,0 +1,73 @@
+package kr.or.dgit.it.datapersistenceapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ProgressBar;
+import android.widget.SeekBar;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+public class WidgetActivity extends AppCompatActivity {
+    ProgressBar mProgBar;
+    ProgressBar mProgCircle;
+
+    SeekBar mSeekBar;
+    TextView mVolume;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_widget);
+        setTitle(getIntent().getStringExtra("title"));
+
+        //progressBar
+        mProgBar = findViewById(R.id.progress);
+        mProgCircle = findViewById(R.id.progcircle);
+
+
+        //SeekBar
+        mSeekBar = findViewById(R.id.seek);
+        mVolume = findViewById(R.id.volume);
+
+        mSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                mVolume.setText("Now Valume : " + progress);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) { }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) { }
+        });
+
+    }
+
+    public void mDecFirstClick(View view) {
+        mProgBar.incrementProgressBy(-2);
+    }
+
+    public void mIncFirstClick(View view) {
+        mProgBar.incrementProgressBy(2);
+    }
+
+    public void mDecSecondClick(View view) {
+        mProgBar.incrementSecondaryProgressBy(-2);
+    }
+
+    public void mIncSecondClick(View view) {
+        mProgBar.incrementSecondaryProgressBy(2);
+    }
+
+    public void mStartClick(View view) {
+        mProgCircle.setVisibility(view.VISIBLE);
+    }
+
+    public void mStopClick(View view) {
+        mProgCircle.setVisibility(view.INVISIBLE);
+    }
+
+}
