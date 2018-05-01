1c2d3d58d7cd3a000511004fc272f39137363185
hanseok Park <es12850@naver.com>
2018-05-01 09:22:55 +0900

    300쪽 미완

--- datapersistenceapplication/src/main/java/kr/or/dgit/it/datapersistenceapplication/SettingPreferenceFragment.java
+++ datapersistenceapplication/src/main/java/kr/or/dgit/it/datapersistenceapplication/SettingPreferenceFragment.java
@@ -20,11 +20,34 @@ public class SettingPreferenceFragment extends PreferenceFragment {
         super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.xml.settings_preference);
 
-        soundPreference = (ListPreference)findPreference("sound_list");
-        keywordSoundPreference = (ListPreference)findPreference("keyword_dound_list");
+        soundPreference = (ListPreference) findPreference("sound_list");
+        keywordSoundPreference = (ListPreference) findPreference("keyword_dound_list");
         keyWordScreen = (PreferenceScreen) findPreference("keyword_screen");
 
         prefs = PreferenceManager.getDefaultSharedPreferences(getActivity());
 
+        if (!prefs.getString("sound_list", "").equals("")) {
+            soundPreference.setSummary(prefs.getString("sound_list", "카톡"));
+        }
+        if (!prefs.getString("keyword_sound_list", "").equals("")) {
+            keywordSoundPreference.setSummary(prefs.getString("keyworld_sound_list", "카톡"));
+        }
+        if (prefs.getBoolean("keyword", false)) {
+            keyWordScreen.setSummary("사용");
+        }
+
+        prefs.registerOnSharedPreferenceChangeListener(prefListener);
     }
+
+    SharedPreferences.OnSharedPreferenceChangeListener prefListener = new SharedPreferences.OnSharedPreferenceChangeListener() {
+        @Override
+        public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+            if (key.equals("sound_list")) {
+                soundPreference.setSummary(prefs.getString("sound_list", "카톡"));
+            }
+            if (key.equals("keyword_sound_list")) {
+                keywordSoundPreference.setSummary(prefs.getString("keyword_sound_list", "카톡"));
+            }
+        }
+    };
 }
